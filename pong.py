5936ff79b77754b7c522b27e883f73c41d0aa35a
mehdios36000 <mehdios36000@github.com>
2020-04-07 23:01:51 +0100

    added  a black window

--- pong.py
+++ pong.py
@@ -3,3 +3,16 @@
 
 
 import turtle
+
+
+
+window=turtle.Screen()
+window.title("pong game is fun")
+window.bgcolor("black")
+window.setup(width=800,height=600)
+window.tracer(0)
+#main game loop
+while True:
+    window.update()
+#paddle A
+
